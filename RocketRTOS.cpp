da020a01e33fb2f03dbb542bcbd6a2fce687abbb
jonse <19jsedgwick@davis.k12.ut.us>
2024-02-24 00:49:51 -0700

    Added dummy tasks for sensors, logging, and stepping

--- RocketRTOS.cpp
+++ RocketRTOS.cpp
@@ -10,10 +10,53 @@ void threadHelloWorld(void){
 
 
 void startRocketRTOS(){
+
+#ifdef LOOP_CHECKIN_TEST
     Scheduler.startLoop(loop2);
     Scheduler.startLoop(loop3);
+#endif //LOOP_CHECKIN_TEST
+
+#ifdef DUMMY_FUNCTIONS
+    Scheduler.startLoop(getSensorsAndDoControlTask);
+    Scheduler.startLoop(logToFileTask);
+    Scheduler.startLoop(moveStepperTask);
+#endif //DUMMY_FUNCTIONS
+
+
 }
 
+
+#ifdef DUMMY_FUNCTIONS
+void loop(){
+
+    yield(); // basically just means the main loop should be ignored
+}
+
+
+void getSensorsAndDoControlTask(){
+    
+    for(volatile unsigned int i=0; i<1000; i++); //waste thousands of clock cycles
+    Serial.println("Sensor Data Aquired");
+
+    delay(SENSOR_PERIOD_MS);
+}
+void logToFileTask(){
+
+    for(volatile unsigned int i=0; i<100000; i++); //waste MORE cycles!
+    Serial.println("Writing to File Complete");
+
+    delay(FILE_PERIOD_MS);
+}
+void moveStepperTask(){
+
+    for(volatile unsigned int i=0; i<100; i++); //waste a few cycles
+    Serial.println("Stepper Moved");
+
+    delay(STEPPER_PERIOD_MS);
+}
+#endif
+
+#ifdef LOOP_CHECKIN_TEST
 void loop2(){
     Serial.println("Loop2, checking in");
     delay(2000);
@@ -24,9 +67,9 @@ void loop3(){
     delay(3000);
 }
 
-
-//this is the main loop. Will this even work or compile?
+//this is the main loop. Will this even work or compile? //it sure does
 void loop(){
   Serial.println("Hiiii! I'm the main thread!");
   delay(1000); 
 }
+#endif //LOOP_CHECKIN_TEST
--- RocketRTOS.hh
+++ RocketRTOS.hh
@@ -27,7 +27,58 @@ void threadHelloWorld(void);
 #include <Scheduler.h>
 
 void startRocketRTOS();
+
+
+//#define LOOP_CHECKIN_TEST
+#ifdef LOOP_CHECKIN_TEST
 void loop2();
 void loop3();
+#endif //LOOP_CHECKIN_TEST
+
+#define DUMMY_FUNCTIONS
+#ifdef DUMMY_FUNCTIONS
+
+#define SENSOR_PERIOD_MS 100
+void getSensorsAndDoControlTask();
+
+#define FILE_PERIOD_MS 2500
+void logToFileTask();
+
+
+/* This would be an implementation with 
+ * moving the stepper as a periodic task.
+ * Ideally, the stepper should also have an
+ * aperiodic component (or be purely aperiodic)
+ * that takes over during any slack between other
+ * tasks. The processor should never be idle
+ * and at this time I'm unsure how to 
+ * over-write the idletask.
+ * 
+ * holup
+ * 
+ * idea
+ * 
+ * what if I treat the main loop as the idle task?
+ * set it up with no delay but have it yield after
+ * every iteration.
+ * holy shit
+ * that might work
+ * 
+ * 
+ * 
+ * 
+ * ALSO we can save clock cycles by having this
+ * task control a pwm hardware timer, but I have
+ * not figured out how to access hardware timers
+ * and identified what they are actually capable 
+ * of just yet. 
+ *
+*/
+#define STEPPER_PERIOD_MS 10
+void moveStepperTask();
+
+
+#endif //DUMMY_FUNCTIONS
+
 
 #endif //ROCKET_RTOS_HH
