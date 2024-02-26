157e113a76c425871771e85da69861c7e106dd5a
jonse <19jsedgwick@davis.k12.ut.us>
2024-02-26 03:57:54 -0700

    Started work on pwm

---
+++ HardwarePWM.cpp
@@ -0,0 +1,12 @@
+#include "HardwarePWM.hh"
+
+#include <nrf.h>
+#include <nrf_pwm.h>
+
+void pwmSetup(){
+    //I need an occiliscope. I'm going to sleep but as soon as I wake up it is time to go to enlab
+}
+
+void pwmPulseNPulses(unsigned long int pulses){
+
+}
---
+++ HardwarePWM.hh
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * HardwarePWM.hh
+ * 
+ * The direct hardware access to PWM and TIMER peripherals necissary to make
+ * the stepper run 100% independently of the CPU
+ * 
+ * NOTE:
+ * The pins are connected as follows
+ * D0 = P1_3 //used for TX
+ * D1 = P1_10 //used for RX
+ * D2 = P1_11
+ * D3 = P1_12
+ * D4 = P1_15
+ * D5 = P1_13
+ * D6 = P1_14
+ * D7 = P0_23
+ * D8 = P0_21
+ * D9 = P0_27
+ * D10 = P1_2
+ * D11 = P1_1 //used for MOSI
+ * D12 = P1_8 //used for MISO
+ * D13 = P0_13 //used for SCK and the LED
+ * 
+ * 02/26/24 - Created File
+******************************************************************************/
+#ifndef HARDWARE_PWM_HH
+#define HARDWARE_PWM_HH
+
+
+//750RPM -> 2500 Steps/s -> 20000 Microsteps/s
+#define PWM_PPS 20000U
+
+void pwmSetup();
+void pwmPulseNPulses(unsigned long int pulses);
+
+
+
+#endif //HARDWARE_PWM_HH
