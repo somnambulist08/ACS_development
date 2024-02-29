6bbb5bf16076ca0374fd683eebc3fcf8972b1703
jonse <19jsedgwick@davis.k12.ut.us>
2024-02-28 20:16:41 -0700

    Implementing State Machine

--- RocketRTOS.cpp
+++ RocketRTOS.cpp
@@ -1,46 +1,120 @@
 #include "RocketRTOS.hh"
 
+//Threads at higher priority than main
 rtos::Thread sensorAndControlThread(osPriorityHigh, THREADS_STACK_DEPTH);
 rtos::Thread loggingThread(osPriorityHigh, THREADS_STACK_DEPTH);
+//Threads at lower priority than main
+rtos::Thread stepperThread(osPriorityLow, THREADS_STACK_DEPTH);
+
+RocketState_t rocketState = ROCKET_BOOT;
 
 void sensorAndControlCallback();
 void loggingCallback();
+void stepperCallback();
+
+//This file provides a definition for deterimining state, but if any other file wants to define it they may
+void determineState() __attribute__((weak));
 
 //Tasks form this file should only be used if they are not defined in any other file
-void stepperTask() __attribute__((weak));
-void sensorAndControlTask() __attribute__((weak));
-void loggingTask() __attribute__((weak));
+void stepper_RUN() __attribute__((weak)); 
+void stepper_CLOSE() __attribute__((weak));
+void stepper_IDLE() __attribute__((weak));
+
+void sensorAndControl_PRE() __attribute__((weak));
+void sensorAndControl_LAUNCH() __attribute__((weak));
+void sensorAndControl_FULL() __attribute__((weak));
+void sensorAndControl_IDLE() __attribute__((weak));
+
+void logging_RUN() __attribute__((weak));
+void logging_CLOSE() __attribute__((weak));
+void logging_IDLE() __attribute__((weak));
 
 void startRocketRTOS(){
+    rocketState = ROCKET_PRE;
     sensorAndControlThread.start(sensorAndControlCallback);
     loggingThread.start(loggingCallback);
-
+    stepperThread.start(stepperCallback);
 }   
 
 void sensorAndControlCallback(){
     while(1){
-        sensorAndControlTask();
+        switch(rocketState){
+            case(ROCKET_PRE):
+                sensorAndControl_PRE();
+                break;
+            case(ROCKET_LAUNCH):
+                sensorAndControl_LAUNCH();
+                break;
+            case(ROCKET_FREEFALL):
+                sensorAndControl_FULL();
+                break;
+            default:
+                sensorAndControl_IDLE();
+                break;
+        }
         delay(SENSOR_AND_CONTROL_DELAY_MS);
     }
 }
 
 void loggingCallback(){
     while(1){
-        loggingTask();
+       switch(rocketState){
+            case(ROCKET_PRE):
+            case(ROCKET_LAUNCH):
+            case(ROCKET_FREEFALL):
+                logging_RUN();
+                break;
+            case(ROCKET_RECOVERY):
+                logging_CLOSE();
+                break;
+            default:
+                logging_IDLE();
+                break;
+        }
         delay(LOGGING_DELAY_MS);
     }
 }
 
-//main loop at a lower priority than the others
-void loop(){
-    while(1){ //technically, the main.cpp wrapps loop. However, doing it here reduces the number of callbacks
-        stepperTask();
+void stepperCallback(){
+    while(1){
+        switch(rocketState){
+            case(ROCKET_FREEFALL):
+                stepper_RUN();
+                break;
+            case(ROCKET_RECOVERY):
+                stepper_CLOSE();
+                break;
+            default:
+                stepper_IDLE();
+                break;
+        }
+        //No delay in lowest-priority thread
     }
+}
+
+//main thread tracks the state
+void loop(){
+    determineState();
+    yield();
+}
+
+//if nobody else defines determinState(), the state is always ROCKET_FREEFALL
+void determineState() {
+    rocketState = ROCKET_FREEFALL;
 }
 
 
 
 //Default Task definitions
-void stepperTask() {while(1) yield();}
-void sensorAndControlTask() {while(1) yield();}
-void loggingTask() {while(1) yield();}
+void stepper_RUN() {stepper_IDLE();}
+void stepper_CLOSE() {stepper_IDLE();}
+void stepper_IDLE() {while(1) yield();}
+
+void sensorAndControl_PRE() {sensorAndControl_IDLE();}
+void sensorAndControl_LAUNCH() {sensorAndControl_IDLE();}
+void sensorAndControl_FULL() {sensorAndControl_IDLE();}
+void sensorAndControl_IDLE() {while(1) yield();}
+
+void logging_RUN() {logging_IDLE();}
+void logging_CLOSE() {logging_IDLE();}
+void logging_IDLE() {while(1) yield();}
--- RocketRTOS.hh
+++ RocketRTOS.hh
@@ -16,16 +16,41 @@
 
 void startRocketRTOS();
 
+//should be defined in another file, else state will allways be ROCKET_FREEFALL
+void determineState();
+
+typedef enum {
+    ROCKET_BOOT = 0,
+    ROCKET_PRE = 1,
+    ROCKET_LAUNCH = 2,
+    ROCKET_FREEFALL = 3,
+    ROCKET_RECOVERY = 4
+} RocketState_t;
+extern RocketState_t rocketState;
+
 #define THREADS_STACK_DEPTH 1024
 //extern to expose them just in case
 extern rtos::Thread sensorAndControlThread;
 extern rtos::Thread loggingThread;
+extern rtos::Thread stepperThread;
 
 #define SENSOR_AND_CONTROL_DELAY_MS 100
 #define LOGGING_DELAY_MS 1000
 
-void stepperTask();
-void sensorAndControlTask();
-void loggingTask();
+//Stepper Tasks
+void stepper_RUN(); //should implement full stepper functionality
+void stepper_CLOSE(); //should close stepper flaps
+void stepper_IDLE(); //called when stepper is not needed
+
+//Sensor and Control Tasks
+void sensorAndControl_PRE(); //should gather data but not integrate or control
+void sensorAndControl_LAUNCH(); //should gather data and integrate but not control
+void sensorAndControl_FULL(); //should gather data and integrate and control 
+void sensorAndControl_IDLE(); //called when sensors and control not needed
+
+//Logging Tasks
+void logging_RUN(); //should log data
+void logging_CLOSE(); //should save the file
+void logging_IDLE(); //called when logging not needed
 
 #endif //ROCKET_RTOS_HH
