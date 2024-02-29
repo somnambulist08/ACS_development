dbe42f4c6f2011cc8ab1893c313ef9c47fe23b44
jonse <19jsedgwick@davis.k12.ut.us>
2024-02-29 14:22:05 -0700

    updated control functions

--- Control.cpp
+++ Control.cpp
@@ -1,24 +1,22 @@
 #include "Control.hh"
-#include <math.h>
 
 
-float integratorState = 0;
 float getDesired(float time) {
-  return 480.0f * powf(M_E, -time * 3.0f / 10.0f) + 1143.0f;
+  return 2200.0f * powf(M_E, -time) + 1219.2f;
 }
 
 
-/*
 float predictAltitude(float height, float velocity){
-  return height + velocity*velocity/2/9.81;
-}*/
+  return height + velocity*velocity/2.0f/9.81f;
+}
 
+float integratorState = 0;
 float getControl(float desired, float predicted, float dt){
   float err = desired - predicted;
-  float control = Pee * err; 
-  integratorState += Iye*err*dt;
+  float control = CONTROL_P * err; 
+  integratorState += CONTROL_I*err*dt;
  
-  control += PI/6;
+  control += CONTROL_BIAS;
   if (control > PI/2) control = PI/2;
   if (control < 0) control = 0;
   return control;
--- Control.hh
+++ Control.hh
@@ -4,27 +4,26 @@
 * Contains all the control functions and constants
 *
 * 02/15/2024 - Created file
+* 02/29/2024 - Updated with newest equations and values
 ******************************************************************************/
 
 
 #ifndef CONTROL_HH
 #define CONTROL_HH
+#include <math.h>
+
 
 float getDesired(float time);
 
-
-
-
-#define predictAltitude(height, velocity) (height + velocity*velocity/2.0/9.81 )
-/*
 float predictAltitude(float height, float velocity);
-*/
+
 #ifndef PI
-#define PI 3.14159265358979323846
+#define PI 3.14159265358979323846f
 #endif
 
-#define Pee -0.0006
-#define Iye -0.0006
+#define CONTROL_P       (-0.001f)
+#define CONTROL_I       (-0.0005f)
+#define CONTROL_BIAS    (PI/8.0f)
  
 extern float integratorState;
 float getControl(float desired, float predicted, float dt);
