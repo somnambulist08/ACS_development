98f61ee7ad06f8d7410e9eb4684d29abb11b1db0
jonse <19jsedgwick@davis.k12.ut.us>
2024-04-09 14:40:45 -0600

    16G fix

--- src/ExternalSensors.cpp
+++ src/ExternalSensors.cpp
@@ -36,15 +36,14 @@ void ExternalSensors::startupTasks(){
   }
 
 //   IMU_a.autoOffsets();
-
+  IMU_a.enableGyrDLPF();
+  IMU_a.enableAccDLPF(true);
+  IMU_a.setAccDLPF(MPU9250_DLPF_0);
   IMU_a.setAccRange(MPU9250_ACC_RANGE_16G);
   IMU_a.setGyrRange(MPU9250_GYRO_RANGE_2000);
   IMU_a.setMagOpMode(AK8963_PWR_DOWN);
-  IMU_a.enableGyrDLPF();
   IMU_a.setGyrDLPF(MPU9250_DLPF_0);
   IMU_a.setSampleRateDivider(8);
-  IMU_a.enableAccDLPF(true);
-  IMU_a.setAccDLPF(MPU9250_DLPF_0);
 
   if(Serial) Serial.println("Calibrating offsets. Don't move");
   calibrateOffsets();
@@ -59,9 +58,9 @@ void ExternalSensors::startupTasks(){
 };
 void ExternalSensors::readAcceleration(float &x, float &y, float &z){
     xyzFloat acc = IMU_a.getGValues();
-    x = acc.x / -8.0f; //I think it's reporting in m/s2 so this is to convert to G
-    y = acc.y / -8.0f; //OR maybe it is just not scaling properly and we need to divide by 8 because 
-    z = (acc.z - 1.0f) / -8.0f; //we went from 2G to 16G range? //ALso, there is a bais on this axis
+    x = -acc.x ;/// -8.0f; //I think it's reporting in m/s2 so this is to convert to G
+    y = -acc.y ;/// -8.0f; //OR maybe it is just not scaling properly and we need to divide by 8 because 
+    z = -(acc.z - 1.0f) ;/// -8.0f; //we went from 2G to 16G range? //ALso, there is a bais on this axis
     x -= accOffsets[0];
     y -= accOffsets[1];
     z -= accOffsets[2];
@@ -71,9 +70,9 @@ void ExternalSensors::readAltitude(float &H){
 };
 void ExternalSensors::readGyroscope(float &x, float &y, float &z){
     xyzFloat gyr = IMU_a.getGyrValues();
-    x=gyr.x / 8.0f; //the gyros also seem to be having a problem with the increased resolution
-    y=gyr.y / 8.0f;
-    z=gyr.z / 8.0f;  
+    x=gyr.x ;/// 8.0f; //the gyros also seem to be having a problem with the increased resolution
+    y=gyr.y ;/// 8.0f;
+    z=gyr.z ;/// 8.0f;  
     x -= gyroOffsets[0];
     y -= gyroOffsets[1];
     z -= gyroOffsets[2];
